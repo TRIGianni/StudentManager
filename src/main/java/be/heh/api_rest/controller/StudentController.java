784690e2e02e1e9adf2be1784c62380fbf908a7e
HeH-Gianni <gianni.tricarico@heh.be>
2026-09-01 14:40:50 +0200

    Ajout d'une réponse personnalisée (ResponseEntity)

--- src/main/java/be/heh/api_rest/controller/StudentController.java
+++ src/main/java/be/heh/api_rest/controller/StudentController.java
@@ -1,20 +1,32 @@
 package be.heh.api_rest.controller;
 
 import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping("/api/")
 public class StudentController {
 
     @PostMapping("students")
-    public void createStudent(@RequestBody @Valid StudentRequest sr) {
-        System.out.println("Fisrtname :" + sr.firstName());
-        System.out.println("LastName :" + sr.lastName());
-        System.out.println("email :" + sr.email());
-        System.out.println("dateOfBirth :" + sr.dateOfBirth());
+    public ResponseEntity<StudentResponse> createStudent(@RequestBody @Valid StudentRequest sr,
+                                                         UriComponentsBuilder uriBuilder) {
+
+        int id = 100;
+        StudentResponse studentResponse = new StudentResponse(id,sr.firstName(),sr.lastName(),
+                sr.email(),sr.dateOfBirth());
+
+        URI location = uriBuilder
+                .path("/api/students/{id}")
+                .buildAndExpand(studentResponse.id())
+                .toUri();
+
+        return ResponseEntity.created(location).body(studentResponse);
     }
 }
---
+++ src/main/java/be/heh/api_rest/controller/StudentResponse.java
@@ -0,0 +1,11 @@
+package be.heh.api_rest.controller;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Size;
+
+import java.time.LocalDate;
+
+public record StudentResponse(int id,String firstName, String lastName, String email, LocalDate dateOfBirth) {
+}
