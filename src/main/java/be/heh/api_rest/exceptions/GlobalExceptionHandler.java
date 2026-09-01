dd3625fb3b74909b5688a02a1b0b8cad5fa04d9a
HeH-Gianni <gianni.tricarico@heh.be>
2026-09-01 10:35:24 +0200

    Ajout gestion des erreurs de validation

---
+++ src/main/java/be/heh/api_rest/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,38 @@
+package be.heh.api_rest.exceptions;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.net.URI;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ProblemDetail handleValidationErrors(MethodArgumentNotValidException ex,
+                                                HttpServletRequest request) {
+
+        ProblemDetail problem = ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST,
+                "Erreur de validation des données");
+        problem.setTitle("Requête invalide");
+        problem.setType(URI.create("http://localhost:8080/api/errors/validation-error"));
+        problem.setInstance(URI.create(request.getRequestURI()));
+
+        List<Map<String,String>> fieldErrors = ex.getBindingResult().
+                getFieldErrors()
+                .stream()
+                .map(fe ->Map.of("field",fe.getField(),"message",
+                        fe.getDefaultMessage()))
+                .collect(Collectors.toList());
+        problem.setProperty("fieldErrors",fieldErrors);
+        return problem;
+    }
+}
